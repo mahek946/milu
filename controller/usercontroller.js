ed62b2ea3d56ccb66e525415f9061d324e28f8d0
mahek946 <vadadoriyamahekvadadoriya@gmail.com>
2025-02-06 15:33:26 +0530

    msg

--- controller/usercontroller.js
+++ controller/usercontroller.js
@@ -1,4 +1,5 @@
 var user = require("../model/usermodel");
+var login_status = 0;
 exports.insert  = async(req,res)=>{
     user.create(req.body);
     res.status(200).json({
@@ -12,20 +13,21 @@ exports.get_data  = async(req,res)=>{
     //     status: "success",
     //     data
     // })
-    var limit = 3
-    var page_no = req.query.page_no;
-    if(page_no==undefined){
-        page_no = 1;
-    }
-    var start = (page_no-1)*limit;
-    var total_reco = await user.find().countDocuments();
-    var total_page = Math.ceil(total_reco/limit);
-    var data = await user.find().skip(start).limit(limit);
+    // var limit = 3
+    // var page_no = req.query.page_no;
+    // if(page_no==undefined){
+    //     page_no = 1;
+    // }
+    // var start = (page_no-1)*limit;
+    // var total_reco = await user.find().countDocuments();
+    // var total_page = Math.ceil(total_reco/limit);
+    // var data = await user.find().skip(start).limit(limit);
+    var data = await user.find();
     res.status(200).json({
         status: "success",
         data,
-        page_no,
-        total_page
+        // page_no,
+        // total_page
     })
 }
 
@@ -54,4 +56,34 @@ exports.delete_data  = async(req,res)=>{
         status: "success",
         data
     })
+}
+
+exports.login = async(req,res)=>{
+    var data = await user.find({"email":req.body.email});
+    if(login_status==0)
+    {
+        if(data.length==1)
+        {
+            if(data[0].password==req.body.password)
+            {
+                login_status = 1;
+                res.status(200).json({
+                status: "login success"
+
+                })
+            }else{
+                res.status(200).json({
+                status: "check your email or password"
+                })
+            }
+        }else{
+            res.status(200).json({
+            status: "check your email or password"
+            })
+        }    
+    }else{
+        res.status(200).json({
+        status: "user is allready login"
+        })
+    }
 }
--- routes/index.js
+++ routes/index.js
@@ -3,9 +3,10 @@ var router = express.Router();
 var mahek = require('../controller/usercontroller');
 
 router.post('/',mahek.insert);
-router.get('/',mahek.get_data);
+router.get('/get',mahek.get_data);
 router.get('/update/:id',mahek.get_update_data);
 router.post('/update/:id',mahek.update_data);
 router.get('/delete/:id',mahek.delete_data);
+router.post('/login',mahek.login);
 
 module.exports = router;
